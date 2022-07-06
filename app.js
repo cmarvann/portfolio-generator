a322e3d3422a9638e52917a67e2703b593a60900
cmarvann <covingtonmarvann@gmail.com>
2022-07-06 18:58:47 -0400

    Validate answers

--- app.js
+++ app.js
@@ -17,12 +17,28 @@ return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username'
+      message: 'Enter GitHub Username (Required)',
+      validate: nameInput => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log('Please enter your GitHub username!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
@@ -48,23 +64,47 @@ return inquirer.prompt([
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
-      },
+        message: 'What is the name of your project? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Enter a project name!');
+          return false;
+        }
+      }
+    },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Enter project description!');
+            return false;
+          }
+        }
       },
       {
         type: 'checkbox',
         name: 'languages',
         message: 'What did you build this project with? (Check all that apply)',
-        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6',  'Node']
       },
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Enter project GitHub link!');
+            return false;
+          }
+        }
       },
       {
         type: 'confirm',
