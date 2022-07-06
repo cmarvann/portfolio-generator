95ff22a4340221d9f721f19c06d109e125cb3295
cmarvann <covingtonmarvann@gmail.com>
2022-07-06 19:36:11 -0400

    Completed feature questions

--- app.js
+++ app.js
@@ -41,12 +41,27 @@ return inquirer.prompt([
       }
     },
     {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
       type: 'input',
       name: 'about',
-      message: 'Provide some information about yourself:'
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      }
     }
-  ]);
-};
+    ]);
+  };
+
+  
   promptUser().then(answers => console.log(answers));
 
   const promptProject = portfolioData => {
